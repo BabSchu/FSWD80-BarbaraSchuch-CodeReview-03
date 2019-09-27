8183839b9400afcdbb949056eefb59d066338934
BabSchu <you@example.com>
2019-09-27 21:18:18 +0200

    add comments

--- js/index.js
+++ js/index.js
@@ -1,37 +1,37 @@
 //version 1:
 
-// function calculateInsurance() {
-// 	var name1 = document.getElementById("name").value;
-// 	var name = name1[0].toUpperCase() + name1.slice(1);
-// 	var age = Number(document.getElementById("age").value);
-// 	var horse_power = Number(document.getElementById("horsepower").value);
-// 	var selCountry = document.getElementById("country");
-// 	var country = selCountry.options[selCountry.selectedIndex].text;
+/*function calculateInsurance() {
+	var name1 = document.getElementById("name").value; //fetches name from input 
+	var name = name1[0].toUpperCase() + name1.slice(1); //converts first charakter to uppercase
+	var age = Number(document.getElementById("age").value); //fetches age from input and converts it to number
+	var horse_power = Number(document.getElementById("horsepower").value); //fetches horsepower from input and converts it to number
+	var selCountry = document.getElementById("country"); //fetches selection country and saves it in a var
+	var country = selCountry.options[selCountry.selectedIndex].text; //fetches option from selection country as text
 
-// 	switch (country) {
-// 		case "Austria":
-// 			insuranceAustria = horse_power*100/age+50;
-// 			document.getElementById("textCalc").innerHTML = 
-// 			name + ", your insurance costs "+ Math.round(insuranceAustria) + " Euro";
-// 			break;
-// 	   case "Hungary":
-// 			insuranceHungary = horse_power*120/age+100;
-// 			document.getElementById("textCalc").innerHTML = 
-// 			name + ", your insurance costs "+ Math.round(insuranceHungary) + " Euro";
-// 			break;
-// 	   case "Greece":
-// 			insuranceGreece = horse_power*150/(age+3)+50;
-// 			document.getElementById("textCalc").innerHTML = 
-// 			name + ", your insurance costs "+ Math.round(insuranceGreece) + " Euro";
-// 			break;
-// 	   default:
-// 			document.getElementById('textCalc').innerHTML= "Please select a country!";
-// 			break;
-// }	
-// };
+	switch (country) {
+		case "Austria":
+			insuranceAustria = horse_power*100/age+50;
+			document.getElementById("textCalc").innerHTML = 
+			name + ", your insurance costs "+ Math.round(insuranceAustria) + " Euro";
+			break;
+	 	case "Hungary":
+			insuranceHungary = horse_power*120/age+100;
+			document.getElementById("textCalc").innerHTML = 
+			name + ", your insurance costs "+ Math.round(insuranceHungary) + " Euro";
+			break;
+	 	case "Greece":
+			insuranceGreece = horse_power*150/(age+3)+50;
+			document.getElementById("textCalc").innerHTML = 
+			name + ", your insurance costs "+ Math.round(insuranceGreece) + " Euro";
+			break;
+		default:
+			document.getElementById('textCalc').innerHTML= "Please select a country!";
+			break;
+}	
+};
 
-// document.getElementById("calcBtn").onclick = calculateInsurance;
-
+document.getElementById("calcBtn").onclick = calculateInsurance;
+*/
 //version 2:
 
 function calculateInsurance () {
